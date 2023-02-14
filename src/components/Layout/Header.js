4092c47cac0f0cfcb2e2c7622780e52e2243b17c
casualjsenthusiast <dharmendranegi998@gmail.com>
2023-02-14 15:11:28 +0530

    Create frontend for a simple food ordering app in React

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+import HeaderCartButton from "./HeaderCartButton";
+import meals from "../../assets/meals.jpg";
+import classes from "./Header.module.css";
+
+const Header = (props) => {
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>Foodie</h1>
+        <HeaderCartButton onClick={props.onShowCart} />
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={meals} alt="Tasty delicious meals" />
+      </div>
+    </>
+  );
+};
+
+export default Header;
---
+++ src/components/UI/Modal.js
@@ -0,0 +1,32 @@
+import classes from "./Modal.module.css";
+import ReactDOM from "react-dom";
+
+const Backdrop = (props) => {
+  return <div onClick={props.onClose} className={classes.backdrop}></div>;
+};
+
+const ModalOverlay = (props) => {
+  return (
+    <div className={classes.modal}>
+      <div className={classes.content}>{props.children}</div>
+    </div>
+  );
+};
+
+const Modal = (props) => {
+  const portalElement = document.getElementById("overlay");
+  return (
+    <>
+      {ReactDOM.createPortal(
+        <Backdrop onClose={props.onClose} />,
+        portalElement
+      )}
+      {ReactDOM.createPortal(
+        <ModalOverlay>{props.children}</ModalOverlay>,
+        portalElement
+      )}
+    </>
+  );
+};
+
+export default Modal;
